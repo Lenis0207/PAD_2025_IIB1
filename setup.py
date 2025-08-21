90e25254306584f0961dcce96d3bd576884d6b49
Ana Maria Lenis <ana.lenis@est.iudigital.edu.co>
2025-08-21 21:47:23 +0000

    setup

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="iudigital_ad",
     version="0.0.1",
-    author="Ana Lenis",
-    author_email="ana.lenis@est.iudigital.edu.co",
+    author="Andres Callejas",
+    author_email="andres.callejas@iudigital.edu.co",
     description="ETL para análisis de datos del dólar",
     py_modules=["actividad1", "actividad2"],
     install_requires=[
